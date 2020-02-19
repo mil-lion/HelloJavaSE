4f79a80450f848e250c3cfd3a03614c87e525a92
Igor Morenko <morenko@lionsoft.ru>
2020-02-19 09:33:33 +0300

    Добавлен пример работы с системными классами System и Runtime

---
+++ src/main/java/ru/lionsoft/javase/hello/HelloSystem.java
@@ -0,0 +1,21 @@
+/*
+ * File:    HelloSystem.java
+ * Project: HelloJavaSE
+ * Date:    18 нояб. 2019 г. 22:38:46
+ * Author:  Igor Morenko <morenko at lionsoft.ru>
+ * 
+ * Copyright 2005-2019 LionSoft LLC. All rights reserved.
+ */
+package ru.lionsoft.javase.hello;
+
+/**
+ * Примеры по работы с системными классами
+ * @author Igor Morenko <morenko at lionsoft.ru>
+ */
+public class HelloSystem {
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
