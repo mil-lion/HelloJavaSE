babc3df9a8489671fc54548784226bb85face66e
Igor Morenko <morenko@lionsoft.ru>
2020-02-19 09:41:49 +0300

    Добавлены примеры по работе с классоим CompletableFuture

--- src/main/java/ru/lionsoft/javase/hello/thread/HelloFuture.java
+++ src/main/java/ru/lionsoft/javase/hello/thread/HelloFuture.java
@@ -9,7 +9,10 @@
 package ru.lionsoft.javase.hello.thread;
 
 import java.util.concurrent.Callable;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.FutureTask;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 /**
  * Пример работы с Callable интерфейсом и асинхронное выполнение функции
@@ -18,6 +21,12 @@ import java.util.concurrent.FutureTask;
 public class HelloFuture {
 
     public static void main(String[] args) throws Exception {
+
+        testFutureTask();
+        testCompletableFuture();
+    }
+    
+    public static void testFutureTask() throws Exception {
         Callable<String> task = () -> {
             try {
                 Thread.sleep(500);
@@ -27,6 +36,36 @@ public class HelloFuture {
         };
         FutureTask<String> future = new FutureTask<>(task);
         new Thread(future).start();
-        System.out.println(future.get());
+        String hello = null;
+        int counter = 0;
+        do {
+            try {
+                hello = future.get(10, TimeUnit.MILLISECONDS);
+            } catch (TimeoutException ex) {
+                System.err.println("Timeout! #" + ++counter);
+            }
+        } while (hello == null);
+        System.out.println("Result: " + hello);
+        
+    }
+    
+    public static void testCompletableFuture() throws Exception {
+        // CompletableFuture уже содержащий результат
+        CompletableFuture<String> completed;
+        completed = CompletableFuture.completedFuture("Просто значение");
+        
+        // CompletableFuture, запускающий (run) новый поток с Runnable, поэтому он Void
+        CompletableFuture<Void> voidCompletableFuture;
+        voidCompletableFuture = CompletableFuture.runAsync(() -> {
+            System.out.println("run " + Thread.currentThread().getName());
+        });
+        
+        // CompletableFuture, запускающий новый поток, результат которого возьмём у Supplier
+        CompletableFuture<String> supplier;
+        supplier = CompletableFuture.supplyAsync(() -> {
+            System.out.println("supply " + Thread.currentThread().getName());
+            return "Значение";
+        });
+        System.out.println(supplier.get());
     }
 }
