3f1bd0c3ff52ecc037dd13da56a23d5c4782c817
Igor Morenko <morenko@lionsoft.ru>
2022-12-06 23:43:53 +0300

    Исправление ошибок

--- src/main/java/ru/lionsoft/javase/hello/HelloSystem.java
+++ src/main/java/ru/lionsoft/javase/hello/HelloSystem.java
@@ -49,15 +49,15 @@ public class HelloSystem {
         
         // *************** System Properties ************
         System.out.println("user.name = " + System.getProperty("user.name"));
-        System.out.println("java.version = " + Integer.getInteger("java.version"));
+        System.out.println("java.vm.specification.version = " + Integer.getInteger("java.vm.specification.version"));
         System.out.println("myprop = " + System.getProperty("myprop", "Unknown"));
         System.out.println("mybool = " + Boolean.getBoolean("mybool"));
-        System.out.println("### System Properties:");
+        System.out.println("\n### System Properties:");
         System.getProperties().forEach((p, v) -> System.out.println(p + "=" + v));
 
         // *************** System Enviroment ************
-        System.out.println("set JAVA_HOME=" + System.getenv("JAVA_HOME"));
-        System.out.println("### System Enviroment:");
+        System.out.println("\nset JAVA_HOME=" + System.getenv("JAVA_HOME"));
+        System.out.println("\n### System Enviroment:");
         System.getenv().forEach((p, v) -> System.out.println(p + "=" + v));
 
         // *************** Runtime *******************
@@ -74,8 +74,8 @@ public class HelloSystem {
                 r.exec("cmd.exe /c dir > c:\\temp\\dir.out");
                 r.exec("cmd.exe /c set > c:\\temp\\set1.out");
             } else {
-                r.exec("ls > ~/tmp/ls.out");
-                r.exec("env > ~/tmp/env1.out");
+                r.exec("ls -l > /Users/mil/tmp/ls.out");
+                r.exec("env > /Users/mil/tmp/env1.out");
             }
         } catch (IOException ex) {
             LOG.log(Level.SEVERE, null, ex);
@@ -88,10 +88,10 @@ public class HelloSystem {
             pb.redirectOutput(new File("c:/temp/set3.out"));
         } else {
             pb = new ProcessBuilder("env");
-            pb.redirectOutput(new File("~/tmp/env3.out"));
+            pb.redirectOutput(new File("/Users/mil/tmp/env3.out"));
         }
-        pb.environment().add("MyEnv1", "MyValue1");
-        pb.environment().add("MyEnv2", "MyValue2");
+        pb.environment().put("MyEnv1", "MyValue1");
+        pb.environment().put("MyEnv2", "MyValue2");
         
         // *************** Exit from Program ***********
         System.exit(0);  // Successfull (or r.exit(0))
